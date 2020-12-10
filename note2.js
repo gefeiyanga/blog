b5aac5bc92b63d613b57476476272a0afed1a0c2
gefeiyanga <variousdid.gmail.com>
2020-12-10 10:22:56 +0800

    test8

--- note2.js
+++ note2.js
@@ -6,3 +6,10 @@ console.log(typeof a)
 console.log(typeof(a))
 let b = 'ss'
 console.log(b.constructor === String)
+
+
+let x = Infinity
+console.log(x == x + 1)
+
+console.log(0.3 - 0.2)
+console.log(0.2 - 0.1)
